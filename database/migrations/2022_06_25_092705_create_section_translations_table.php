d3ce0a26d04aaced55809255e390253e3f535130
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-25 13:36:45 +0300

    - Create Section Controller, Model & Migration - Create SectionTranslation Model & Migration

---
+++ database/migrations/2022_06_25_092705_create_section_translations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectionTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('section_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('section_id')->constrained('sections')->onDelete('cascade');
+            $table->string('locale')->index();
+            $table->string('section_name');
+            $table->unique(['section_id', 'locale']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('section_translations');
+    }
+}
