0d66d2c68811213abc643cca861d697f2bb147b6
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-25 15:21:51 +0300

    Implement All  Functions in this Controller

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Section;
 use Illuminate\Http\Request;
+use App\Http\Traits\SaveImageTrait;
+use Illuminate\Support\Facades\File;
 
 class SectionController extends Controller
 {
+    use SaveImageTrait;
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +19,8 @@ class SectionController extends Controller
      */
     public function index()
     {
-        //
+        $sections = Section::translated()->get();
+        return view('Admin.Section.section', compact('sections'));
     }
 
     /**
@@ -25,7 +30,7 @@ class SectionController extends Controller
      */
     public function create()
     {
-        //
+        return view('Admin.Section.add_section');
     }
 
     /**
@@ -36,18 +41,39 @@ class SectionController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'section_name_ar'       => 'required|string|min:3|max:20|unique:section_translations,section_name',
+                'section_name_en'       => 'required|string|min:3|max:20|unique:section_translations,section_name',
+                'icon_name'             => 'required|image|mimes:jpg,jpeg,png,gif',
+                'photo_name'            => 'required|image|mimes:jpg,jpeg,png,gif',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect('admin/sections/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $icon_name = $this->saveImage($request->file('icon_name'), 'images/Section', 20, 20);
+        $photo_name = $this->saveImage($request->file('photo_name'), 'images/Section', 200, 200);
+        $data = [
+            'icon_name'           => $icon_name,
+            'photo_name'          => $photo_name,
+            'ar' => [
+                'section_name'    => $request['section_name_ar'],
+            ],
+            'en' => [
+                'section_name'    => $request['section_name_en'],
+            ],
+        ];
+        Section::create($data);
+
+        session()->flash('Add', 'تم إضافة الفئة الرئيسية بنجاح');
+        return redirect('admin/sections');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Section  $section
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Section $section)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -57,7 +83,7 @@ class SectionController extends Controller
      */
     public function edit(Section $section)
     {
-        //
+        return view('Admin.Section.edit_section');
     }
 
     /**
@@ -67,9 +93,51 @@ class SectionController extends Controller
      * @param  \App\Models\Section  $section
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Section $section)
+    public function update(Request $request, $id)
     {
-        //
+        $section = Section::find($id);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'section_name_ar'       => 'required|string|min:3|max:20|unique:section_translations,section_name',
+                'section_name_en'       => 'required|string|min:3|max:20|unique:section_translations,section_name',
+                'icon_name'             => 'required|image|mimes:jpg,jpeg,png,gif',
+                'photo_name'            => 'required|image|mimes:jpg,jpeg,png,gif',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return redirect('admin/sections/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        // Update The Image
+        if ($request->hasFile('icon_name') && $request->hasFile('photo_name')) {
+            $icon_destination  =  $section->icon_name;
+            $photo_destination =  $section->photo_name;
+            if (File::exists($icon_destination) && File::exists($photo_destination)) {
+                File::delete($icon_destination);
+                File::delete($photo_destination);
+            }
+            $icon_name = $this->saveImage($request->file('icon_name'), 'images/Section', 20, 20);
+            $photo_name = $this->saveImage($request->file('photo_name'), 'images/Section', 200, 200);
+        }
+
+        $data = [
+            'icon_name'           => $icon_name,
+            'photo_name'          => $photo_name,
+            'ar' => [
+                'section_name'    => $request['section_name_ar'],
+            ],
+            'en' => [
+                'section_name'    => $request['section_name_en'],
+            ],
+        ];
+        $section->update($data);
+
+        session()->flash('Add', 'تم تعديل الفئة الرئيسية بنجاح');
+        return redirect('admin/sections');
     }
 
     /**
@@ -78,8 +146,19 @@ class SectionController extends Controller
      * @param  \App\Models\Section  $section
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Section $section)
+    public function destroy($id)
     {
-        //
+        $section = Section::find($id);
+        $icon_destination =  $section->icon_name;
+        if (File::exists($icon_destination)) {
+            File::delete($icon_destination);
+        }
+        $photo_destination =  $section->photo_name;
+        if (File::exists($photo_destination)) {
+            File::delete($photo_destination);
+        }
+        $section->delete();
+        session()->flash('delete', 'تم حذف الفئة الرئيسية بنجاح');
+        return redirect('admin/sections');
     }
 }
