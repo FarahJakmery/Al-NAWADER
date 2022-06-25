c685826012bc046befec3a7943b7364c68a2c5a8
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-25 15:34:54 +0300

    Add Some Routes To web,php File

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\Admin\SectionController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin.Auth.login');
+});
+
+
+// ============================ Admin Routes ============================
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function () {
+        Route::view('/login', 'Admin.Auth.login')->name('login');
+        Route::post('/check', [AdminController::class, 'check'])->name('check');
+    });
+
+    Route::middleware(['auth:admin', 'PreventBackHistory'])->group(function () {
+        // Authentication Routes
+        Route::view('/home', 'Admin.Auth.dashboard')->name('home');
+        Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
+        // Section Route
+        Route::resource('sections', SectionController::class);
+        // Category Route
+        Route::resource('categories', CategoryController::class);
+    });
 });
