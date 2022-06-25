5bbbbed92737b18d4a19474af077d5591f62eb55
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-25 13:38:40 +0300

    Add a Relationship To join User Table With DeviceTokens Table

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the device_tokens for the user.
+     */
+    public function deviceTokens()
+    {
+        return $this->hasMany(DeviceToken::class);
+    }
 }
