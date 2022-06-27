9c8869f57636ff4ef4400b60e0801d1e50578d1f
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-27 21:42:28 +0300

    Add Some Codes Here

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Section;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Sharing Data With All Views
+        View::share('sections', Section::translated()->get());
+        View::share('categories', Category::translated()->get());
     }
 }
