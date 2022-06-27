53de7d8ca6506fc82111e8b97da06896b59df7cb
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-27 21:35:05 +0300

    Add a Function For Saving Multiple Image

--- app/Http/Traits/SaveImageTrait.php
+++ app/Http/Traits/SaveImageTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Traits;
 
+use App\Models\AdsImage;
 use Intervention\Image\ImageManagerStatic as Image;
 
 trait SaveImageTrait
@@ -14,5 +15,20 @@ trait SaveImageTrait
         $image_name_DataBase = $folder . '/' . time() . '.' . $imageExtension;
         Image::make($photo)->resize($width, $height)->save($folder . '/' . $image_name, 60);
         return $image_name_DataBase;
+    }
+
+    public function saveAdvertisementImage($Image, $folder, $width, $height, $advertisement)
+    {
+        // Store Image In Public Folder
+        $imageExtension = $Image->getClientOriginalExtension();
+        $image_name =  md5(rand(1000, 100000)) .  '.' . $imageExtension;
+        $image_name_DataBase = $folder . '/' . $image_name;
+        Image::make($Image)->resize($width, $height)->save($folder . '/' . $image_name, 60);
+
+        // Store Image In Data_Base
+        AdsImage::create([
+            'advertisement_id'  => $advertisement->id,
+            'image_name'        => $image_name_DataBase,
+        ]);
     }
 }
