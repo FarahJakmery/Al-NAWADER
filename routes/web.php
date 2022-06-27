b47862016808b1b0ac4afadad2d8151ceb299703
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-27 21:37:01 +0300

    Make Some Changes On web.php File

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\SectionController;
 use Illuminate\Support\Facades\Route;
 
