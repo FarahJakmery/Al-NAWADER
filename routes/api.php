6d3d1b9a4a7935091efb5f8e1ce23108067a70b8
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-29 12:26:12 +0300

    Add Some Routes To api.php file

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AdvertisementController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ReplyController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
@@ -18,14 +19,16 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 Route::get('advertisements', [AdvertisementController::class, 'allAdvertisements']);
 // Show a Specific Advertisement by Id Route
 Route::get('show_advertisement/{id}', [AdvertisementController::class, 'showAdvertisement']);
-// Add  Advertisement Route
-Route::post('add_advertisement', [AdvertisementController::class, 'storeAdvertisement']);
-// Update Advertisement Route
-Route::put('update_advertisement/{id}', [AdvertisementController::class, 'updateAdvertisement']);
-// Delete Advertisement Route
-Route::delete('remove_advertisement/{id}', [AdvertisementController::class, 'removeAdvertisements']);
-// My Advertisement Route
-Route::get('my_advertisements', [AdvertisementController::class, 'myAdvertisement']);
 
 Route::middleware(['jwt.verify'])->group(function () {
+    // Advertisements Routes
+    Route::post('add_advertisement', [AdvertisementController::class, 'storeAdvertisement']);
+    Route::post('update_advertisement/{id}', [AdvertisementController::class, 'updateAdvertisement']);
+    Route::delete('remove_advertisement/{id}', [AdvertisementController::class, 'removeAdvertisements']);
+    Route::get('my_advertisements', [AdvertisementController::class, 'myAdvertisement']);
+
+    // Replies Routes
+    Route::post('add_reply', [ReplyController::class, 'addReply']);
+    Route::post('update_reply/{id}', [ReplyController::class, 'updateReply']);
+    Route::delete('remove_reply/{id}', [ReplyController::class, 'removeReply']);
 });
