9db4946c7d68a565fa25b9fbfac141802faf6a60
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-07-02 10:26:36 +0300

    Create Social Media Model & Migration

---
+++ database/migrations/2022_06_30_132515_create_social_media_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_media', function (Blueprint $table) {
+            $table->id();
+            $table->string('Facebook');
+            $table->string('WhatsApp');
+            $table->string('Instagram');
+            $table->string('YouTube');
+            $table->string('Telegram');
+            $table->string('LinkedIn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_media');
+    }
+}
