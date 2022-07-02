c2bd535baa4085a41e220beda0e85da33de616ba
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-07-02 09:50:20 +0300

    Add Some New Routes To api.php File

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\Api\AdvertisementController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CommissionController;
+use App\Http\Controllers\Api\PageController;
 use App\Http\Controllers\Api\ReplyController;
+use App\Http\Controllers\Api\SectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
@@ -15,10 +19,23 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::put('/user-update-profile', [AuthController::class, 'update']);
 });
 
-// All Advertisements Route
+// Sections Routes
+Route::get('all_sections', [SectionController::class, 'allSections']);
+Route::get('show_section/{id}', [SectionController::class, 'showSection']);
+
+// Categories Routes
+Route::get('all_categories', [CategoryController::class, 'allCategories']);
+Route::get('show_category/{id}', [CategoryController::class, 'showCategory']);
+
+// Advertisements Routes
 Route::get('advertisements', [AdvertisementController::class, 'allAdvertisements']);
-// Show a Specific Advertisement by Id Route
 Route::get('show_advertisement/{id}', [AdvertisementController::class, 'showAdvertisement']);
+
+//Pages Routes
+Route::get('privacy_policy_page', [PageController::class, 'privacyPolicyPage']);
+Route::get('share_application_page', [PageController::class, 'shareApplicationPage']);
+Route::get('social_media', [PageController::class, 'contactUsPage']);
+Route::post('sent_message_to_email', [PageController::class, 'sent_message_to_email']);
 
 Route::middleware(['jwt.verify'])->group(function () {
     // Advertisements Routes
@@ -31,4 +48,8 @@ Route::middleware(['jwt.verify'])->group(function () {
     Route::post('add_reply', [ReplyController::class, 'addReply']);
     Route::post('update_reply/{id}', [ReplyController::class, 'updateReply']);
     Route::delete('remove_reply/{id}', [ReplyController::class, 'removeReply']);
+
+    // Commission Routes
+    Route::post('send_commission_request', [CommissionController::class, 'commissionRequest']);
+    Route::get('all_commission_request/{id}', [CommissionController::class, 'allCommissionRequest']);
 });
